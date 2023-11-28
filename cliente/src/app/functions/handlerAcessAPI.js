2a51b3df4f01fdbd0dce26256cc992b8567374b0
karolpanuci <karolainny.panucci@estudante.ifms.edu.br>
2023-11-28 16:31:43 -0400

    adicionando codigo correto

---
+++ cliente/src/app/functions/handlerAcessAPI.js
@@ -0,0 +1,69 @@
+'use server'
+
+
+const url = "http://localhost:400"
+
+
+const getUserAuthenticated = async (user) => {
+    const responseOfApi = await fetch(url +"/user/authenticated",
+    {
+       cache:"no-cache",
+       method:"POST",
+       headers:{"Content-Type":"Application/json"},
+       body: JSON.stringify(user)
+    }
+    );
+   const userAuth =await responseOfApi.json();
+   console.log(userAuth)
+   return userAuth;
+}
+
+const getUsers = async () =>{
+    try{
+        const responseOfApi = await fetch(url + "/users",{
+            next:{revalidate:10}
+        });
+        const listUsers = await responseOfApi.json();
+
+        return listUsers
+    }catch{
+        return null;
+    }
+
+    }
+
+    const postUser = async (user) =>{
+        try{
+            const responseOfApi = await fetch(url + "/user", {
+                method: "POST",
+                headers: { 'Content-Type': 'Application/json'},
+                body: JSON.stringify(user)
+            });
+            const userSave = await responseOfApi.json();
+            return userSave;
+        }catch{
+           return null;
+        }
+    }
+    const updateUser = async (user, id ) => {
+        const token = cookies().get('token')?.value;
+
+        try {
+            const responseOfApi = await fetch(`${url}/user/${id}`, { 
+                method:'PUT',
+                headers: {
+                    'Content-type': 'Application/json',
+                    cookies: `token=${token}`
+                },
+                body: JOSON.stringify(user)
+            
+        });
+
+        const userSave = await responseOfApi.json();
+        return userSave;
+    }catch{
+        return null;
+    }}
+
+export { getUsers, getUserAuthenticated, postUser, updateUser };
+
---
+++ cliente/src/app/pages/dashboard/page.js
@@ -0,0 +1,19 @@
+import { getUsers } from "../../functions/handlerAcessAPI";
+import { Suspense } from "react";
+import ListUsers from "../../componente/ListUsers";
+import './estilo.css';
+
+export default async function Dashboard() {
+    const users = await getUsers();
+
+    return (
+        <div>
+            <Suspense fallback={<p>carregando...</p>}>
+                <ListUsers users={users} />
+
+                <a href="/pages/alterar">Alterar</a>
+                <a href="/pages/registrar">Registrar</a>
+            </Suspense>
+        </div>
+    );
+}
